3587c30beb4248f033a3094ba7f0b580c69bd0e2
venkatAkurathi <venkat.akurathi@gmail.com>
2023-12-21 23:22:31 +0000

    csproj issue

--- BPCalculatorTests/GlobalUsings.cs
+++
@@ -1,4 +0,0 @@
-global using Microsoft.Playwright.MSTest;
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using System.Text.RegularExpressions;
-global using System.Threading.Tasks;
